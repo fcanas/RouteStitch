368f8833b8c894153e0bf81a44b1faaecd6f3630
Fabian Canas <fcanas@gmail.com>
2014-11-10 13:20:37 -0500

    Initial Commit

---
+++ RouteStitch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RouteStitch
+//
+//  Created by Fabian Canas on 11/10/14.
+//  Copyright (c) 2014 Fabian Canas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
